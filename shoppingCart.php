4d00aee4978d927cdf82c6cda5c7f908f7d27a77
Jason <jimanproductions@gmail.com>
2022-11-11 12:51:47 +0100

    updated todo, started reading json file

--- shoppingCart.php
+++ shoppingCart.php
@@ -1,6 +1,6 @@
 <?php
 include "functions.php";
-
+include "classes/class_Order.php";
 displayNavBar();
 
 ?>
@@ -19,6 +19,12 @@ displayNavBar();
     <?php 
     
     // Displays the current items in the shopping cart. 
+    $jsonDataFromFile = file_get_contents("./data/shoppingCart.json");
+    $jsonObj = json_decode($jsonDataFromFile);
+
+    var_dump($jsonObj)
+
+
     // Cookie can be used to store product ID and quantity of the products in the shopping cart. 
     // Add a button for "Pay", when clicked, a form should appear for the customer to fill in his details, with a final button named "confirm Pay", which adds the order onto the database. 
     // After the order has been added to the database, cookie for the shopping cart should be destroyed. 
